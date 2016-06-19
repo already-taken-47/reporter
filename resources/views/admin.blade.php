faeaad61b730f6597df505590f03d3e690cb82c5
kir <kirillvidyuk7@gmail.com>
2016-06-19 16:06:58 +0300

    More initialization

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kir
+ * Date: 19.06.16
+ * Time: 16:06
+ */
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <title>Laravel</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <script type="text/javascript" src="{{ asset('js/lib/knockout-3.4.0.js') }}" ></script>
 
         <style>
             html, body {
